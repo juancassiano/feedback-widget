aaf57ba206e765e864c3576108c518c7866effcd
Juan Cassiano <juancassiano@hotmail.com>
2022-05-03 12:14:27 -0300

    end of day 2

--- src/components/WidgetForm/index.tsx
+++ src/components/WidgetForm/index.tsx
@@ -1,4 +1,5 @@
 import { CloseButton } from "../CloseButton";
+
 import bugImageUrl from "../../assets/bug.svg";
 import ideaImageUrl from "../../assets/idea.svg";
 import thoughtImageUrl from "../../assets/thought.svg";
@@ -9,24 +10,24 @@ import { FeedbackSuccessStep } from "./Steps/FeedbackSuccessStep";
 
 export const feedbackTypes = {
   BUG: {
-    title: "Bug",
+    title: "Problema",
     image: {
       source: bugImageUrl,
-      alt: "Imagem de um inseto",
+      alt: "Ilustração de um inseto roxo",
     },
   },
   IDEA: {
     title: "Ideia",
     image: {
       source: ideaImageUrl,
-      alt: "Imagem de uma lâmpada",
+      alt: "Lâmpada acesa",
     },
   },
   OTHER: {
     title: "Outro",
     image: {
       source: thoughtImageUrl,
-      alt: "Imagem de um balão de pensamento",
+      alt: "Núvem de pensamento",
     },
   },
 };
@@ -45,7 +46,7 @@ export function WidgetForm() {
   return (
     <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
       {feedbackSent ? (
-        <FeedbackSuccessStep />
+        <FeedbackSuccessStep onFeedbackRestartRequest={handleRestartFeedback} />
       ) : (
         <>
           {!feedbackType ? (
@@ -59,11 +60,14 @@ export function WidgetForm() {
           )}
         </>
       )}
-
       <footer className="text-xs text-neutral-400">
-        Feito com ♥ pela
-        <a className="underline-offset-2" href="http://www.rocketseat.com.br" />
-        Rocketseat
+        Feito com ♥ pela{" "}
+        <a
+          href="https://rocketseat.com.br"
+          className="underline underline-offset-2"
+        >
+          Rocketseat
+        </a>
       </footer>
     </div>
   );
